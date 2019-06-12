aa7e8c4f7600579daa6680b17a1cc273ba5711d1
7mioda <ahmededaly1993@gmail.com>
2019-06-13 01:31:20 +0200

    adding some dependencies

---
+++ src/Controller/TestController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+
+class TestController extends FOSRestController {
+/**
+*
+* @Rest\Get("/test")
+* @return View
+*/
+public function testAction(): View
+{
+return View::create(['test' => 'OK']);
+}
+}
