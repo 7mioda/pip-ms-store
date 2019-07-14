35b0498e6856aba986c9f2e987ce9fa3a714ebe4
7mioda <ahmededaly1993@gmail.com>
2019-07-14 15:27:57 +0200

    updating mailer service

--- src/Controller/Mailer.php
+++ src/Controller/Mailer.php
@@ -2,9 +2,12 @@
 
 
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Twig\Environment;
 use Swift_Mailer;
 use Swift_Message;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 
 class Mailer 
@@ -15,7 +18,7 @@ class Mailer
     private $fromEmail;
 
 
-    public function __construct(Swift_Mailer $mailer, \Twig_Environment $templating)
+    public function __construct(Swift_Mailer $mailer,Environment $templating)
     {
         $this->mailer = $mailer;
         $this->templating = $templating;
@@ -23,26 +26,65 @@ class Mailer
     }
 
 
+    /**
+     * @param $template
+     * @param array $option
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
     protected function setTemplate($template, array $option)
     {
-
         return $this->templating->render($template, $option);
+
     }
 
-    public function sendResettingEmailMessage(array $infoUser)
+    /**
+     * @param array $infoUser
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function  sendResettingEmailMessage(array $infoUser)
     {
         $subject = '[plantify.com] Réinitialisation de votre mot de passe';
-        $body = $this->setTemplate('Mailer/reset_password.html.twig', array('username' => 'amed eddaly', 'confirmationLink' => 'this is test below', 'isfirstLogin' => false));
-        $this->sendEmailMessage('mmekni.a.amine@gmail.com', $subject, $body);
+        $body = $this->setTemplate(
+            'Mailer/reset_password.html.twig',
+            ['username' => $infoUser['username'], 'confirmationLink' => $infoUser['confirmationLink']]);
+        $this->sendEmailMessage($infoUser['email'], $subject, $body);
+    }
+
+    /**
+     * @param array $infoUser
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function sendInscriptionEmailMessage(array $infoUser)
+    {
+        $subject = 'Bienvenue sur plantify.com !';
+        $body = $this->setTemplate('Mailer/reset_password.html.twig',
+            ['firstLogin' => true, 'identifier' => $infoUser['identifier'],
+                'link' => $infoUser['link']]);
+        $this->sendEmailMessage($infoUser['email'], $subject, $body);
     }
 
 
-    // public function sendFirstLoginEmailMessage(array $infoUser)
-    // {
-    //     $subject = 'Bienvenue sur plantify.com !';
-    //     $body = $this->setTemplate('Mailer/reset_password.html.twig', array('isfirstLogin' => true, 'identifiant' => $infoUser['identifiant'],'link' => $infoUser['link']));
-    //     $this->sendEmailMessage('mmekni.a.amine@gmail.com', $subject, $body);
-    // }
+    /**
+     * @param array $infoUser
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function sendFirstLoginEmailMessage(array $infoUser)
+     {
+         $subject = 'Bienvenue sur plantify.com !';
+         $body = $this->setTemplate('Mailer/reset_password.html.twig',
+             ['firstLogin' => true, 'identifier' => $infoUser['identifier'],
+                 'link' => $infoUser['link']]);
+         $this->sendEmailMessage($infoUser['email'], $subject, $body);
+     }
 
 
     protected function sendEmailMessage($toEmail, $subject, $body)
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,18 +3,31 @@ namespace App\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
-use App\Controller\Mailer;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Error\Error;
+
 
 class TestController extends FOSRestController {
-/**
-*
-* @Rest\Get("/test")
-* @return View
-*/
-public function testAction(Mailer $mailer): View
-{
-$stripeClient = $this->get('flosch.stripe.client');
-$mailer->sendResettingEmailMessage([]);
-return View::create(['test' => 'OK', 'stripe' => $stripeClient]);
-}
+    /**
+     *
+     * @Rest\Get("/test")
+     * @param Mailer $mailer
+     * @return View
+     */
+    public function testAction(Mailer $mailer): View
+    {
+        $stripeClient = $this->get('flosch.stripe.client');
+        try {
+            $mailer->sendResettingEmailMessage(
+                [
+                    'username' => 'username',
+                    'confirmationLink' => 'confirmationLink',
+                    'email' => 'ahmededaly1993@gmail.com'
+                ]
+            );
+        } catch (Error $e) {
+            return View::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return View::create(['test' => 'OK', 'stripe' => $stripeClient]);
+    }
 }
