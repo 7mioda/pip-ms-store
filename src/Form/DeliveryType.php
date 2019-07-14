45835cf541cd781eb409483015cbe6b5c6f68102
7mioda <ahmededaly1993@gmail.com>
2019-07-14 14:01:19 +0200

    updating delivery

--- src/Form/DeliveryType.php
+++ src/Form/DeliveryType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Delivery;
+use App\Entity\Order;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,25 @@ class DeliveryType extends AbstractType
     {
         $builder
             ->add('status')
-            ->add('order')
+            ->add('address')
+            ->add('coordinateX')
+            ->add('coordinateY')
+            ->add('createdAt', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('deliveredAt', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('order',  EntityType::class, [
+                'choice_label' => 'id',
+                'by_reference' => true,
+                'expanded' => true,
+                'class'    => Order::class,
+                'query_builder' => function(\Doctrine\ORM\EntityRepository $er) {
+                    $qb = $er->createQueryBuilder('d');
+                    return $qb->orderBy('d.id', 'ASC');
+                }
+            ])
         ;
     }
 
@@ -21,6 +42,8 @@ class DeliveryType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Delivery::class,
+            'csrf_protection' => false,
+            'validation_groups' => false,
         ]);
     }
 }
